03719db819ed737ff6317d522e07d5b49e0d1e66
Dustin Smith <Administrator@Dustins-MacBook-Pro.local>
2014-02-01 18:02:53 -0600

    random improvements

--- public/js/add.js
+++ public/js/add.js
@@ -1,9 +1,21 @@
 $(function() {
-	$("body").on('keydown', 'textarea', function() {
-		if($(this).val() === '' && $(this).parent().is(":last-child")) {
+	$("body").on('keyup', 'textarea', function() {
+		var val = $(this).val();
+		var isLast = $(this).parent().is(":last-child");
+
+		if (val !== '' && isLast) {
 			var parentClone = $(this).parent().clone();
 			parentClone.find("textarea").val("");
 			parentClone.appendTo($(this).parent().parent());
+		}
+		else if (val === '' && !isLast) {
+			var nextTextAreas = $(this).parent().nextAll().find("textarea");
+			var emptyTextAreas = nextTextAreas.filter(function() {
+				return $(this).val() === '';
+			});
+			if (emptyTextAreas.length === nextTextAreas.length) { // if all of the following text boxes are empty remove the last one
+				nextTextAreas.last().parent().remove();
+			}
 		}
 	});
 
@@ -11,20 +23,33 @@ $(function() {
 	form.find("button").on('click', function (ev) {
 		ev.preventDefault();
 
+		var textareas = form.find("textarea");
+		var valid = textareas.filter(function () {
+			return $.trim($(this).val()) !== '';
+		}).length > 0;
+
+		if(!valid) {
+			alert("C'mon...At least type in a message!");
+			return false;
+		}
+
 		$.post(form.attr("action"), 
 			form.serialize(), 
 			function (resp) {
 				showResult(resp.data.shareUrl);
 			});
-
 	});
 
+	var $body = $("body");
 	$(".color-picker").spectrum({
+		clickoutFiresChange: true,
 		change: function(color) {
 			var hex = color.toHexString();
 			$(this).val(hex);
-			$("body").css($(this).data("cssprop"), hex);
+			$body.css($(this).data("cssprop"), hex);
 		}
+	}).each(function() {
+		$(this).spectrum('set', $body.css($(this).data("cssprop")));
 	});
 
 	//var addContainer = $("#add-container");
--- routes/custom.js
+++ routes/custom.js
@@ -1,5 +1,10 @@
 module.exports = function (app) {
 
+	app.get('/Jen', function (req, res) {
+		res.render('view', {
+			messages: ['Hi Jen', "How's it going?"]
+		});
+	});
 
 
 };
